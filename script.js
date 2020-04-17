752cde4522baea1dd4ccd7b863d0f77bd5bb3a38
sarahebrooks12 <sarahebrooks12@gmail.com>
2020-04-17 19:35:49 -0400

    begin

---
+++ script.js
@@ -0,0 +1,9 @@
+// In your JavaScript file, declare a new variable called likesToTravel and set it equal to true or false.
+// If likesToTravel is true, log "Bon voyage!" to the console. If not, log "Enjoy your couch!" to the console.'
+
+var likesToTravel = false;
+if (likesToTravel === true) {
+    console.log("Bon Voyage!")
+} else {
+    console.log("You gonna love COVID 19")
+}
